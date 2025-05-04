49b1530812d62f47649ff3435d77f7cad85c3dc3
[kaylmatyu] <[calingasankylematthew@gmail.com]>
2025-05-04 17:30:06 +0800

    update uli

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Sir Bry Ipasa mo na ako, hahahah SHALOM PO
       </p>
     </>
   )
